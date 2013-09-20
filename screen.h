542d195b72cee02e9c5d97543eae6bfe1e92bee6
Shockk <shokku.ra@gmail.com>
2013-09-20 20:15:30 +0100

    Wrote SCREEN_XYTOL macro.

--- screen.h
+++ screen.h
@@ -5,6 +5,8 @@
 #define SCREEN_Y 25
 #define SCREEN_SIZE (SCREEN_X * SCREEN_Y)
 
+#define SCREEN_XYTOL(X, Y) ((X) + (Y) * SCREEN_X)
+
 void screen_cursor_to(unsigned short);
 void screen_clear(void);
 
