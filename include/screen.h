2485265c67271a1dbff7f551053959c21d3b1746
Shockk <shokku.ra@gmail.com>
2014-05-23 14:08:47 +0100

    Added missing screen function definitions, removed explicit null terminator

--- include/screen.h
+++ include/screen.h
@@ -9,6 +9,8 @@ extern unsigned short screen_cursor_loc(void);
 extern void screen_cursor_to(unsigned short);
 extern void screen_cursor_by(unsigned short);
 extern void screen_clear(void);
+extern void screen_writec(char, unsigned short);
 extern void screen_putc(char);
+extern void screen_puts(char *);
 
 #endif
--- main.c
+++ main.c
@@ -4,7 +4,7 @@ void entry(void)
 {
 	screen_clear();
 	screen_cursor_to(0);
-	screen_puts("ShockkOS v0.1.0\0");
+	screen_puts("ShockkOS v0.1.0");
 
 	// loop
 	for(;;);
