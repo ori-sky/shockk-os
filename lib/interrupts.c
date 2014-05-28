f298edf4599faafeed03e048849a0241d21f56fd
Shockk <shokku.ra@gmail.com>
2014-05-28 01:56:20 +0100

    Print key up/down messages

--- lib/interrupts.c
+++ lib/interrupts.c
@@ -7,6 +7,7 @@
 struct cpu_state * interrupts_callback(struct cpu_state *state)
 {
 	char str[32];
+	unsigned char scancode;
 
 	switch(state->interrupt)
 	{
@@ -16,9 +17,11 @@ struct cpu_state * interrupts_callback(struct cpu_state *state)
 		case IRQ0:
 			break;
 		case IRQ1:
-			uitoa(ports_inb(0x60), str, 2);
-			screen_puts("scancode 0b");
-			screen_puts(str);
+			scancode = ports_inb(0x60);
+			screen_puts("key ");
+			screen_puts(scancode & 0x80 ? "up" : "down");
+			screen_puts(": ");
+			screen_puts(uitoa(scancode & 0x7F, str, 16));
 			screen_putc('\n');
 			break;
 	}
