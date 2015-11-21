869d6873a53e90dafd702fd64a642849846b5c8c
Shockk <shokku.ra@gmail.com>
2015-11-21 09:15:43 +0000

    Enable interrupts before initializing GDT

--- main.c
+++ main.c
@@ -7,14 +7,14 @@
 #include "screen.h"
 
 void entry(void) {
+	struct IDT *idt = (struct IDT *)0x500;
+	struct GDT *gdt = (struct GDT *)(0x500 + sizeof(struct GDT));
+
 	pic_remap(IRQ0, IRQ8);
 	pic_set_masks(0, 0);
-	idt_init((struct IDT *)(0x500));
-
-	gdt_init((struct GDT *)(0x500 + sizeof(struct IDT)));
-
+	idt_init(idt);
 	__asm__ __volatile__ ("sti");
-
+	gdt_init(gdt);
 	screen_init();
 
 	__asm__ __volatile__ ("int $0x50");
