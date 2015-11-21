894cc166986c67c670ce9be8a06242d1311484aa
Shockk <shokku.ra@gmail.com>
2015-11-21 15:00:13 +0000

    Correctly set TSS data selector

--- tss.c
+++ tss.c
@@ -4,7 +4,7 @@ void tss_init(volatile struct TSS *tss) {
 	for(unsigned int i = 0; i < sizeof(struct TSS); ++i) {
 		((unsigned char *)tss)[i] = 0;
 	}
-	tss->ss0 = 0x8;
+	tss->ss0 = 0x10;
 	tss->esp0 = 0x70000;
 	__asm__ __volatile__ ("ltr %%ax" : : "a" (0x2B)); /* 0x28 (5th segment) OR'd with 0x3 (ring 3) */
 }
