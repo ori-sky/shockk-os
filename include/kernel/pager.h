3e42d819bf323824db8e67d21417ff39568eaf2d
Shockk <shokku.ra@gmail.com>
2015-11-27 21:52:46 +0000

    Flesh out directory and table structures

--- include/kernel/pager.h
+++ include/kernel/pager.h
@@ -3,12 +3,40 @@
 
 #include <stdint.h>
 
-struct PageDirectory {
-	uint32_t tables[1024];
+union PageTableEntry {
+	void *address;
+	struct {
+		uint8_t present       : 1;
+		uint8_t writable      : 1;
+		uint8_t unprivileged  : 1;
+		uint8_t write_through : 1;
+		uint8_t disable_cache : 1;
+		uint8_t accessed      : 1;
+		uint8_t dirty         : 1;
+		uint8_t reserved      : 1;
+	} __attribute__((packed)) flags;
 } __attribute__((packed));
 
 struct PageTable {
-	uint32_t pages[1024];
+	union PageTableEntry pages[1024];
+} __attribute__((packed));
+
+union PageDirectoryEntry {
+	struct PageTable *address;
+	struct {
+		uint8_t present       : 1;
+		uint8_t writable      : 1;
+		uint8_t unprivileged  : 1;
+		uint8_t write_through : 1;
+		uint8_t disable_cache : 1;
+		uint8_t accessed      : 1;
+		uint8_t ignored_6     : 1;
+		uint8_t page_size     : 1;
+	} __attribute__((packed)) flags;
+} __attribute__((packed));
+
+struct PageDirectory {
+	union PageDirectoryEntry tables[1024];
 } __attribute__((packed));
 
 struct Pager {
--- kernel/pager.c
+++ kernel/pager.c
@@ -4,4 +4,9 @@
 void pager_init(struct Pager *pager, struct PageAllocator *allocator) {
 	pager->allocator = allocator;
 	pager->directory = page_allocator_alloc(allocator);
+
+	for(unsigned int table = 0; table < 1024; ++table) {
+		pager->directory->tables[table].address = NULL;
+		pager->directory->tables[table].flags.writable = 1;
+	}
 }
