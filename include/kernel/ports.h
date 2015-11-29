ce24f0dfc8f2fc3c61076d136a5ec82132130cc6
Shockk <shokku.ra@gmail.com>
2015-11-29 02:52:05 +0000

    Write 16-bit ports functions

--- include/kernel/ports.h
+++ include/kernel/ports.h
@@ -4,8 +4,10 @@
 #include <stdint.h>
 
 uint8_t ports_inb(unsigned short port);
+uint16_t ports_ins(unsigned short port);
 uint32_t ports_inl(unsigned short port);
 void ports_outb(unsigned short port, uint8_t data);
+void ports_outs(unsigned short port, uint16_t data);
 void ports_outl(unsigned short port, uint32_t data);
 
 #endif
--- kernel/ports.c
+++ kernel/ports.c
@@ -6,6 +6,12 @@ uint8_t ports_inb(unsigned short port) {
 	return result;
 }
 
+uint16_t ports_ins(unsigned short port) {
+	uint16_t result;
+	__asm__ __volatile__ ("in %1, %0" : "=a" (result) : "d" (port));
+	return result;
+}
+
 uint32_t ports_inl(unsigned short port) {
 	uint32_t result;
 	__asm__ __volatile__ ("in %1, %0" : "=a" (result) : "d" (port));
@@ -13,6 +19,10 @@ uint32_t ports_inl(unsigned short port) {
 }
 
 void ports_outb(unsigned short port, uint8_t data) {
+	__asm__ ("out %0, %1" : : "a" (data), "d" (port));
+}
+
+void ports_outs(unsigned short port, uint16_t data) {
 	__asm__ ("out %0, %1" : : "a" (data), "d" (port));
 }
 
