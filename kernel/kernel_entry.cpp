3be2fe378cc083fc18adc4efbc2ec81463dcf7fb
David Farrell <shokku.ra@gmail.com>
2018-09-01 03:05:37 +0100

    Remove debug output/panics

--- kernel/kernel_entry.cpp
+++ kernel/kernel_entry.cpp
@@ -14,13 +14,7 @@ extern "C" void user_enter(void *) __attribute__((noreturn));
 extern "C" void kernel_entry(Pager *) __attribute__((noreturn));
 void kernel_entry(Pager *pager) {
 	screen_init();
-	screen_put('S');
-	screen_put('H');
-	screen_put('K');
-	screen_put('\n');
 	screen_print("SHK\n");
-
-	kernel_panic("in kernel mode (1)");
 
 	pit_set(1 << 15); // programmable timer
 	pic_remap(IRQ0, IRQ8);
@@ -35,8 +29,6 @@ void kernel_entry(Pager *pager) {
 	TSS *tss = (TSS *)pager->Reserve();
 	gdt_init(gdt, tss);
 	tss_init(tss, pager);
-
-	kernel_panic("in kernel mode");
 
 	unsigned char *user_stack = (unsigned char *)pager->Alloc();
 	user_enter(&user_stack[PAGE_ALLOCATOR_PAGE_SIZE]);
