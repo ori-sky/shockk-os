dd48c0f713f877502dd6d4a3008656911475ad6a
David Farrell <shokku.ra@gmail.com>
2018-09-03 04:57:20 +0100

    Move ELF stuff into header

--- loader/loader_entry.cpp
+++ loader/loader_entry.cpp
@@ -1,96 +1,13 @@
 #include <stdint.h>
 #include <arch/x86/a20.h>
-#include <kernel/ext2.h>
 #include <kernel/ata.h>
+#include <kernel/elf.h>
+#include <kernel/ext2.h>
 #include <kernel/mbr.h>
 #include <kernel/pager.h>
 #include <kernel/itoa.h>
 #include <kernel/panic.h>
 #include <kernel/screen.h>
-
-enum class ELFIdentVersion : uint8_t {
-	None = 0,
-	ELF1 = 1
-};
-
-enum class ELFIdentClass : uint8_t {
-	None = 0,
-	x32  = 1,
-	x64  = 2
-};
-
-enum class ELFIdentEncoding : uint8_t {
-	None         = 0,
-	LittleEndian = 1,
-	BigEndian    = 2
-};
-
-enum class ELFIdentABI : uint8_t {
-	SystemV = 0x00,
-	Linux   = 0x03,
-	SHK     = 0x7F
-};
-
-enum class ELFType : uint16_t {
-	None        = 0,
-	Relocatable = 1,
-	Executable  = 2,
-	Dynamic     = 3,
-	Core        = 4,
-};
-
-enum class ELFMachine : uint16_t {
-	None     = 0,
-	Intel386 = 3
-};
-
-struct ELFIdent {
-	uint8_t          magic[4];
-	ELFIdentClass    class_;
-	ELFIdentEncoding encoding;
-	ELFIdentVersion  version;
-	ELFIdentABI      abi;
-	uint8_t          abi_version;
-	uint8_t          padding[7];
-} __attribute__((packed));
-
-struct ELFHeader {
-	ELFIdent   ident;
-	ELFType    type;
-	ELFMachine machine;
-	uint32_t   version;   // we can just use ELFIdent.version which is a uint8_t
-	uint32_t   entry_ptr;
-	uint32_t   ph_offset; // program header offset
-	uint32_t   sh_offset; // section header offset
-	uint32_t   flags;
-	uint16_t   elf_header_size;
-	uint16_t   ph_size;
-	uint16_t   ph_count;
-	uint16_t   sh_size;
-	uint16_t   sh_count;
-	uint16_t   sh_str_idx;
-} __attribute__((packed));
-
-enum class ELFProgramType : uint32_t {
-	Null    = 0,
-	Load    = 1,
-	Dynamic = 2,
-	Interp  = 3,
-	Note    = 4,
-	Shared  = 5,
-	Header  = 6
-};
-
-struct ELFProgramHeader {
-	ELFProgramType type;
-	uint32_t offset;
-	uint32_t v_addr;
-	uint32_t p_addr;
-	uint32_t file_size;
-	uint32_t mem_size;
-	uint32_t flags;
-	uint32_t align;
-} __attribute__((packed));
 
 #define SCREEN_CASE(X)       case X:  return screen << #X
 #define SCREEN_DEFAULT(X, Y) default: return screen << #X << "::<INVALID> (" << static_cast<uint32_t>(Y) << ')'
