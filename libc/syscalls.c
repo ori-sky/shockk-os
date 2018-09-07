9f79245308daa1ad2e8a6f20a5a14cbe5024cba1
David Farrell <shokku.ra@gmail.com>
2018-09-07 13:48:56 +0100

    Fix syscall definitions

--- libc/syscalls.c
+++ libc/syscalls.c
@@ -5,16 +5,17 @@
 #include <sys/times.h>
 #include <sys/errno.h>
 #include <sys/time.h>
+#include <sys/unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-register caddr_t stack_ptr asm ("sp");
+register void *stack_ptr asm ("sp");
 
 void _exit();
 
 int close(int file) {
 	char sz[] = "NOT IMPLEMENTED: close\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	return -1;
 }
@@ -22,9 +23,9 @@ int close(int file) {
 char *__env[1] = {0};
 char **environ = __env;
 
-int execve(char *name, char **argv, char **env) {
+int execve(const char *name, char * const argv[], char * const env[]) {
 	char sz[] = "NOT IMPLEMENTED: execve\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	errno = ENOMEM;
 	return -1;
@@ -32,7 +33,7 @@ int execve(char *name, char **argv, char **env) {
 
 int fork() {
 	char sz[] = "NOT IMPLEMENTED: fork\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	errno = EAGAIN;
 	return -1;
@@ -40,7 +41,7 @@ int fork() {
 
 int fstat(int file, struct stat *st) {
 	char sz[] = "NOT IMPLEMENTED: fstat\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	st->st_mode = S_IFCHR;
 	return 0;
@@ -48,51 +49,51 @@ int fstat(int file, struct stat *st) {
 
 int getpid() {
 	char sz[] = "NOT IMPLEMENTED: getpid\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	return 1;
 }
 
 int isatty(int file) {
 	char sz[] = "NOT IMPLEMENTED: isatty\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	return 1;
 }
 
 int kill(int pid, int sig) {
 	char sz[] = "NOT IMPLEMENTED: kill\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	errno = EINVAL;
 	return -1;
 }
 
-int link(char *old, char *new) {
+int link(const char *old, const char *new) {
 	char sz[] = "NOT IMPLEMENTED: link\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	errno = EMLINK;
 	return -1;
 }
 
-int lseek(int file, int ptr, int dir) {
+off_t lseek(int file, off_t ptr, int dir) {
 	char sz[] = "NOT IMPLEMENTED: lseek\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	return 0;
 }
 
 int open(const char *name, int flags, ...) {
 	char sz[] = "NOT IMPLEMENTED: open\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	return -1;
 }
 
-int read(int file, char *ptr, int len) {
+_READ_WRITE_RETURN_TYPE read(int file, void *ptr, size_t len) {
 	char sz[] = "NOT IMPLEMENTED: read\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	return 0;
 }
@@ -100,28 +101,28 @@ int read(int file, char *ptr, int len) {
 // reserve 32K for stack
 #define STACK_BUFFER (4096 * 32)
 
-caddr_t sbrk(int incr) {
+void * sbrk(ptrdiff_t incr) {
 	char sz[] = "NOT IMPLEMENTED: sbrk\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	extern char _end;
 
-	static caddr_t heap_ptr = (caddr_t)&_end;
+	static void *heap_ptr = (void *)&_end;
 
 	if((stack_ptr - (heap_ptr + incr)) > STACK_BUFFER) {
-		caddr_t base = heap_ptr;
+		void *base = heap_ptr;
 		heap_ptr += incr;
 		return base;
 	} else {
 		errno = ENOMEM;
-		return (caddr_t)-1;
+		return (void *)-1;
 	}
 }
 
 /*
 caddr_t sbrk(int incr) {
 	char sz[] = "NOT IMPLEMENTED: sbrk\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	extern char _end;
 	static char *heap_end;
@@ -145,7 +146,7 @@ caddr_t sbrk(int incr) {
 
 int stat(const char *file, struct stat *st) {
 	char sz[] = "NOT IMPLEMENTED: stat\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	st->st_mode = S_IFCHR;
 	return 0;
@@ -153,14 +154,14 @@ int stat(const char *file, struct stat *st) {
 
 clock_t times(struct tms *buf) {
 	char sz[] = "NOT IMPLEMENTED: times\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	return -1;
 }
 
-int unlink(char *name) {
+int unlink(const char *name) {
 	char sz[] = "NOT IMPLEMENTED: unlink\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	errno = ENOENT;
 	return -1;
@@ -168,18 +169,23 @@ int unlink(char *name) {
 
 int wait(int *status) {
 	char sz[] = "NOT IMPLEMENTED: wait\n";
-	write(stdout, sz, sizeof(sz) - 1);
+	write(STDERR_FILENO, sz, sizeof(sz) - 1);
 
 	errno = ECHILD;
 	return -1;
 }
 
-int write(int file, char *ptr, int len) {
-	for(int n = 0; n < len; ++n) {
-		syscall_put(ptr[n]);
+_READ_WRITE_RETURN_TYPE write(int file, const void *ptr, size_t len) {
+	if(file == STDOUT_FILENO || file == STDERR_FILENO) {
+		const char *sz = ptr;
+		for(int n = 0; n < len; ++n) {
+			syscall_put(sz[n]);
+		}
+		return len;
+	} else {
+		errno = EIO;
+		return -1;
 	}
-	errno = EIO;
-	return -1;
 }
 
 //int gettimeofday(struct timeval * __restrict__ p, void *z);
