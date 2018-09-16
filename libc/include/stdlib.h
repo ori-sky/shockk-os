755b94ccf39d67d54e0d4374e5799c819b0be89d
David Farrell <shokku.ra@gmail.com>
2018-09-16 02:25:35 +0100

    Add stub for free

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -1,6 +1,11 @@
 #ifndef _STDLIB_H
 #define _STDLIB_H 1
 
+#include <stddef.h>
+
+void free(void *);
+void * malloc(size_t);
+
 _Noreturn void exit(int);
 
 #endif
--- libc/src/stdlib.c
+++ libc/src/stdlib.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void free(void *ptr) {
+	
+}
+
+void * malloc(size_t size) {
+	return NULL;
+}
+
 _Noreturn void exit(int status) {
 	fprintf(stderr, "return code = %d\n", status);
 	fflush(stdout);
