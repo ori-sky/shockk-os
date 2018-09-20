5debf14d631d66c6e4167fb1bdf5c6e274880bfb
David Farrell <shokku.ra@gmail.com>
2018-09-20 14:03:05 +0100

    libc: Write some ctype functions

---
+++ libc/src/ctype.c
@@ -0,0 +1,125 @@
+#include <ctype.h>
+
+int isalnum(int c) {
+	return isalpha(c) || isdigit(c);
+}
+
+int isalpha(int c) {
+	return isupper(c) || islower(c);
+}
+
+int isdigit(int c) {
+	switch(c) {
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+		return 1;
+	default:
+		return 0;
+	}
+}
+
+int islower(int c) {
+	switch(c) {
+	case 'a':
+	case 'b':
+	case 'c':
+	case 'd':
+	case 'e':
+	case 'f':
+	case 'g':
+	case 'h':
+	case 'i':
+	case 'j':
+	case 'k':
+	case 'l':
+	case 'm':
+	case 'n':
+	case 'o':
+	case 'p':
+	case 'q':
+	case 'r':
+	case 's':
+	case 't':
+	case 'u':
+	case 'v':
+	case 'w':
+	case 'x':
+	case 'y':
+	case 'z':
+		return 1;
+	default:
+		return 0;
+	}
+}
+
+int isupper(int c) {
+	switch(c) {
+	case 'A':
+	case 'B':
+	case 'C':
+	case 'D':
+	case 'E':
+	case 'F':
+	case 'G':
+	case 'H':
+	case 'I':
+	case 'J':
+	case 'K':
+	case 'L':
+	case 'M':
+	case 'N':
+	case 'O':
+	case 'P':
+	case 'Q':
+	case 'R':
+	case 'S':
+	case 'T':
+	case 'U':
+	case 'V':
+	case 'W':
+	case 'X':
+	case 'Y':
+	case 'Z':
+		return 1;
+	default:
+		return 0;
+	}
+}
+
+int isxdigit(int c) {
+	switch(c) {
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+	case 'a':
+	case 'b':
+	case 'c':
+	case 'd':
+	case 'e':
+	case 'f':
+	case 'A':
+	case 'B':
+	case 'C':
+	case 'D':
+	case 'E':
+	case 'F':
+		return 1;
+	default:
+		return 0;
+	}
+}
