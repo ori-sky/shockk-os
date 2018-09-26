298256a38785406ca0b8da8dccd52d877c313d18
David Farrell <shokku.ra@gmail.com>
2018-09-26 01:11:47 +0100

    Lower-case scancode keys

--- kernel/kb.cpp
+++ kernel/kb.cpp
@@ -31,25 +31,25 @@ char kb_scancode1_char(unsigned char scancode) {
 		return '\t';
 
 	case 0x10:
-		return 'Q';
+		return 'q';
 	case 0x11:
-		return 'W';
+		return 'w';
 	case 0x12:
-		return 'E';
+		return 'e';
 	case 0x13:
-		return 'R';
+		return 'r';
 	case 0x14:
-		return 'T';
+		return 't';
 	case 0x15:
-		return 'Y';
+		return 'y';
 	case 0x16:
-		return 'U';
+		return 'u';
 	case 0x17:
-		return 'I';
+		return 'i';
 	case 0x18:
-		return 'O';
+		return 'o';
 	case 0x19:
-		return 'P';
+		return 'p';
 	case 0x1A:
 		return '[';
 	case 0x1B:
@@ -58,23 +58,23 @@ char kb_scancode1_char(unsigned char scancode) {
 		return '\n';
 
 	case 0x1E:
-		return 'A';
+		return 'a';
 	case 0x1F:
-		return 'S';
+		return 's';
 	case 0x20:
-		return 'D';
+		return 'd';
 	case 0x21:
-		return 'F';
+		return 'f';
 	case 0x22:
-		return 'G';
+		return 'g';
 	case 0x23:
-		return 'H';
+		return 'h';
 	case 0x24:
-		return 'J';
+		return 'j';
 	case 0x25:
-		return 'K';
+		return 'k';
 	case 0x26:
-		return 'L';
+		return 'l';
 	case 0x27:
 		return ';';
 	case 0x28:
@@ -85,19 +85,19 @@ char kb_scancode1_char(unsigned char scancode) {
 	case 0x2B:
 		return '\\';
 	case 0x2C:
-		return 'Z';
+		return 'z';
 	case 0x2D:
-		return 'X';
+		return 'x';
 	case 0x2E:
-		return 'C';
+		return 'c';
 	case 0x2F:
-		return 'V';
+		return 'v';
 	case 0x30:
-		return 'B';
+		return 'b';
 	case 0x31:
-		return 'N';
+		return 'n';
 	case 0x32:
-		return 'M';
+		return 'm';
 	case 0x33:
 		return ',';
 	case 0x34:
@@ -120,27 +120,27 @@ char kb_scancode_char(unsigned char scancode) {
 	case 0x0E:
 		return '`';
 	case 0x15:
-		return 'Q';
+		return 'q';
 	case 0x16:
 		return '1';
 	case 0x1A:
-		return 'Z';
+		return 'z';
 	case 0x1B:
-		return 'S';
+		return 's';
 	case 0x1C:
-		return 'A';
+		return 'a';
 	case 0x1D:
-		return 'W';
+		return 'w';
 	case 0x1E:
 		return '2';
 	case 0x21:
-		return 'C';
+		return 'c';
 	case 0x22:
-		return 'X';
+		return 'x';
 	case 0x23:
-		return 'D';
+		return 'd';
 	case 0x24:
-		return 'E';
+		return 'e';
 	case 0x25:
 		return '4';
 	case 0x26:
@@ -148,33 +148,33 @@ char kb_scancode_char(unsigned char scancode) {
 	case 0x29:
 		return ' ';
 	case 0x2A:
-		return 'V';
+		return 'v';
 	case 0x2B:
-		return 'F';
+		return 'f';
 	case 0x2C:
-		return 'T';
+		return 't';
 	case 0x2D:
-		return 'R';
+		return 'r';
 	case 0x2E:
 		return '5';
 	case 0x31:
-		return 'N';
+		return 'n';
 	case 0x32:
-		return 'B';
+		return 'b';
 	case 0x33:
-		return 'H';
+		return 'h';
 	case 0x34:
-		return 'G';
+		return 'g';
 	case 0x35:
-		return 'Y';
+		return 'y';
 	case 0x36:
 		return '6';
 	case 0x3A:
-		return 'M';
+		return 'm';
 	case 0x3B:
-		return 'J';
+		return 'j';
 	case 0x3C:
-		return 'U';
+		return 'u';
 	case 0x3D:
 		return '7';
 	case 0x3E:
@@ -182,11 +182,11 @@ char kb_scancode_char(unsigned char scancode) {
 	case 0x41:
 		return ',';
 	case 0x42:
-		return 'K';
+		return 'k';
 	case 0x43:
-		return 'I';
+		return 'i';
 	case 0x44:
-		return 'O';
+		return 'o';
 	case 0x45:
 		return '0';
 	case 0x46:
@@ -196,11 +196,11 @@ char kb_scancode_char(unsigned char scancode) {
 	case 0x4A:
 		return '/';
 	case 0x4B:
-		return 'L';
+		return 'l';
 	case 0x4C:
 		return ';';
 	case 0x4D:
-		return 'P';
+		return 'p';
 	case 0x4E:
 		return '-';
 	case 0x52:
